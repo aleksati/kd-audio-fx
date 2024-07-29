7993a8396c1cba81d528dce702665b2d6024aafa
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-07-29 13:57:59 +0200

    Update Models.py

--- Code/Models.py
+++ Code/Models.py
@@ -7,7 +7,7 @@ Initializes a data generator object
   :param batch_size: The size of each batch returned by __getitem__
 """
 
-def create_model_LSTM(units, input_dim=1, conditioning_size=0, b_size=2399):
+def _create_model_LSTM(units, input_dim=1, conditioning_size=0, b_size=2399):
 
     # Defining inputs
     inputs = tf.keras.layers.Input(batch_shape=(b_size, 1, input_dim), name='input')
@@ -15,6 +15,47 @@ def create_model_LSTM(units, input_dim=1, conditioning_size=0, b_size=2399):
     outputs = tf.keras.layers.LSTM(units, stateful=True, return_sequences=False, return_state=False, name='LSTM')(
         inputs)
     outputs = tf.keras.layers.Dense(units//2, name='Linear')(outputs)
+
+    if conditioning_size != 0:
+        cond_inputs = tf.keras.layers.Input(batch_shape=(b_size, conditioning_size), name='cond_inputs')
+
+    outputs = tf.keras.layers.Dense(1, name='OutLayer')(outputs)
+
+    if conditioning_size != 0:
+        model = tf.keras.models.Model([inputs, cond_inputs], outputs)
+    else:
+        model = tf.keras.models.Model(inputs, outputs)
+
+    model.summary()
+    return model
+
+
+
+def create_model_LSTM(units, input_dim=1, conditioning_size=0, b_size=2399):
+
+    # Defining inputs
+    inputs = tf.keras.layers.Input(batch_shape=(b_size, 1, input_dim), name='input')
+
+    outputs = tf.keras.layers.LSTM(4, stateful=True, return_sequences=True, return_state=False, name='LSTM')(
+        inputs)
+
+    outputs = tf.keras.layers.LSTM(8, stateful=True, return_sequences=True, return_state=False, name='LSTM1')(
+        outputs)
+
+    outputs = tf.keras.layers.LSTM(16, stateful=True, return_sequences=True, return_state=False, name='LSTM2')(
+        outputs)
+
+    outputs = tf.keras.layers.LSTM(32, stateful=True, return_sequences=True, return_state=False, name='LSTM3')(
+        outputs)
+
+    outputs = tf.keras.layers.LSTM(16, stateful=True, return_sequences=True, return_state=False, name='LSTM4')(
+        outputs)
+
+    outputs = tf.keras.layers.LSTM(8, stateful=True, return_sequences=True, return_state=False, name='LSTM5')(
+        outputs)
+
+    outputs = tf.keras.layers.LSTM(4, stateful=True, return_sequences=False, return_state=False, name='LSTM6')(
+        outputs)
 
     if conditioning_size != 0:
         cond_inputs = tf.keras.layers.Input(batch_shape=(b_size, conditioning_size), name='cond_inputs')
