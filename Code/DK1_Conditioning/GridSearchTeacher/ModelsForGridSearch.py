672147e7183f7a97f646a6bc5bb6fdf911c35eee
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-10-31 16:59:01 +0100

    adding the saving and loading of output layer of the teacher into the student

--- Code/DK1_Conditioning/GridSearchTeacher/ModelsForGridSearch.py
+++ Code/DK1_Conditioning/GridSearchTeacher/ModelsForGridSearch.py
@@ -7,42 +7,28 @@ Initializes a data generator object
   :param batch_size: The size of each batch returned by __getitem__
 """
 
-
-def create_model_LSTM_DK1(input_dim=1, conditioning_size=0, b_size=2399):
+def create_model_LSTM_DK1(input_dim=1, conditioning_size=1, b_size=2400):
 
     # Defining inputs
     inputs = tf.keras.layers.Input(
-        batch_shape=(b_size, input_dim), name='input')
+        batch_shape=(b_size, 1, input_dim), name='input')
+
     cond_inputs = tf.keras.layers.Input(batch_shape=(b_size, conditioning_size), name='cond_inputs')
-    inputs = tf.expand_dims(inputs, axis=1)
 
+    outputs = tf.keras.layers.LSTM(8, stateful=True, return_sequences=True, name="LSTM")(inputs)
 
-    outputs = tf.keras.layers.LSTM(16, stateful=True, return_sequences=True, name='LSTM1')(
-        inputs)
+    outputs = tf.keras.layers.LSTM(32, stateful=True, return_sequences=True, name='LSTM2')(outputs)
 
-    outputs = FiLM(in_size=16)(outputs[:, :, 0], cond_inputs)
-    outputs = tf.expand_dims(outputs, axis=1)
-
-    outputs = tf.keras.layers.LSTM(32, stateful=True, return_sequences=True, name='LSTM2')(
-        outputs)
+    outputs = tf.keras.layers.LSTM(32, stateful=True, return_sequences=True, name='LSTM3')(outputs)
 
     outputs = FiLM(in_size=32)(outputs[:, :, 0], cond_inputs)
     outputs = tf.expand_dims(outputs, axis=1)
 
-    outputs = tf.keras.layers.LSTM(32, stateful=True, return_sequences=True, name='LSTM4')(
-        outputs)
-
-    outputs = FiLM(in_size=32)(outputs[:, :, 0], cond_inputs)
-    outputs = tf.expand_dims(outputs, axis=1)
-
-    outputs = tf.keras.layers.LSTM(16, stateful=True, return_sequences=False, name='LSTM5')(
-        outputs)
-
-    outputs = FiLM(in_size=16)(outputs, cond_inputs)
+    outputs = tf.keras.layers.LSTM(8, stateful=True, return_sequences=False, name="LSTM4")(outputs)
 
     outputs = tf.keras.layers.Dense(1, name='OutLayer')(outputs)
-    model = tf.keras.models.Model([cond_inputs, inputs], outputs)
 
+    model = tf.keras.models.Model([cond_inputs, inputs], outputs)
 
     model.summary()
 
@@ -50,7 +36,7 @@ def create_model_LSTM_DK1(input_dim=1, conditioning_size=0, b_size=2399):
 
 
 
-def __create_model_LSTM_DK1(input_dim=1, conditioning_size=0, b_size=2399):
+def __create_model_LSTM_DK1(input_dim=1, conditioning_size=1, b_size=2399):
 
     # Defining inputs
     inputs = tf.keras.layers.Input(
